e8ad9f4230783ed19f8c9a93192c3327eee06152
abdullah azad <4947845+abazad@users.noreply.github.com>
2020-12-21 03:54:49 +0300

    Update paymes.php
    whmcs'deki varsayilan para birimi TL değil ise müşteri TL ile ödeme yaptıktan sonra gelen tutarı whmcs varsayilan para biriminde zannediyor.
    
    Örnek:
    10 EUR Invoice ödüyor müşteri
    95 euro ödemis gibi gösteriyor ve müşteriye 85 euro credit balance ekliyor.

--- modules/gateways/callback/paymes.php
+++ modules/gateways/callback/paymes.php
@@ -21,6 +21,25 @@ $currency = $_POST["currency"];
 $message = $_POST["message"];
 $success = $_POST['status'] == '3DS_ENROLLED';
 
+/**
+ * bu kodun calismasi icin TL'nin whmcs'de varsayilan para birimi "olmamasi" gerek. (not: paymes api basvurum onaylanmadigi icin henuz test edemedim)
+ */
+$result = mysql_fetch_assoc(select_query('tblinvoices', 'total, userid', array("id"=>$invoiceId))); // faturanin tutarini ve musteri id'sini alalım.
+$amount = $result['total'];
+# paymes'den gelen tutar fatura'daki tutar ile ayni mi? degilse whmcs'deki varsayilan para birimine cevirelim.
+//$currency = getCurrency();
+$result = mysql_fetch_assoc(select_query('tblclients', 'currency', array("id"=>$result['userid']))); // musterinin para birimini aldik
+$currency_id = $result['currency'];
+$result = mysql_fetch_array(select_query("tblcurrencies", "id", array("id"=>1))); // whmcs'deki varsayilan para birimin id'sini aldik.
+$default_id = $result['id'];
+if($currency_id != $default_id) { // musterinin para birimi ile whmcs'deki varsayilan para birimi ayni degilse
+    $converted_amount = convertCurrency($amount, $currency_id, $default_id); // gelen tutari whmcs'deki varsayilan para birimine cevirdik
+    $paymentAmount = $converted_amount; // whmcs gelen tutari dogru bir sekilde faturaya yazsin diye degiskene atadik.
+} else {
+    $converted_amount = $amount; // musterinin para birimi ile whmcs'deki varsayilan para birimi zaten ayniymis.
+    $paymentAmount = $converted_amount; // whmcs gelen tutari dogru bir sekilde faturaya yazsin diye degiskene atadik.
+}
+
 $transactionStatus = $success ? 'Success' : 'Failure';
 
 $invoiceId = checkCbInvoiceID($invoiceId, $gatewayParams['name']);
